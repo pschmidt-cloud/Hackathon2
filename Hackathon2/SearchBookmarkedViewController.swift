87106244c3d3261d1dfd6012881aa7854473e26d
Paul Schmidt <pschmidt.mdr@gmail.com>
2014-08-20 12:42:51 -0700

    Updates

---
+++ Hackathon2/SearchBookmarkedViewController.swift
@@ -0,0 +1,9 @@
+//
+//  SearchBookmarkedViewController.swift
+//  Hackathon2
+//
+//  Created by Paul Schmidt on 8/18/14.
+//  Copyright (c) 2014 Paul Schmidt. All rights reserved.
+//
+
+import Foundation
---
+++ Hackathon2/SearchViewController.swift
@@ -0,0 +1,13 @@
+//
+//  SearchViewController.swift
+//  Hackathon2
+//
+//  Created by Paul Schmidt on 8/19/14.
+//  Copyright (c) 2014 Paul Schmidt. All rights reserved.
+//
+
+import Cocoa
+
+class SearchViewController: UIViewController {
+
+}
